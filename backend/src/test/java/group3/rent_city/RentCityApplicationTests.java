37fd5289075a9d9e8ea3861f82ef0675beeb2de7
Admin <duytinh.vn@gmail.com>
2026-03-13 19:51:35 +0700

    1

---
+++ backend/src/test/java/group3/rent_city/RentCityApplicationTests.java
@@ -0,0 +1,13 @@
+package group3.rent_city;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
